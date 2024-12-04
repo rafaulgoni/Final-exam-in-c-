e9432d3a2294c7de210eab32e74d3f3d903f5bf7
unknown <rafaulgoni787898@gmail.com>
2024-12-04 22:28:05 +0600

    test github

--- code.cpp
+++ code.cpp
@@ -1,44 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-//five number...
-struct Student {
-    string name;
-    int cls;
-    char section;
-    int id;
-    int math_marks;
-    int eng_marks;
-};
-
-bool compare(const Student &a, const Student &b) {
-    if (a.eng_marks == b.eng_marks) {
-        if (a.math_marks == b.math_marks) {
-            return a.id < b.id;
-        }
-        return a.math_marks > b.math_marks;
-    }
-    return a.eng_marks > b.eng_marks;
-}
-
 int main() {
     int N;
     cin >> N;
-    Student students[N];
-    
-    for (int i = 0; i < N; ++i) {
-        cin >> students[i].name >> students[i].cls >> students[i].section >> students[i].id >> students[i].math_marks >> students[i].eng_marks;
+    int A[N];
+
+    // Reading the array elements
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
     }
-    
-    sort(students, students + N, compare);
-    
-    for (int i = 0; i < N; ++i) {
-        cout << students[i].name << " " << students[i].cls << " " << students[i].section << " " << students[i].id << " " << students[i].math_marks << " " << students[i].eng_marks << endl;
+
+    // Printing the array in reverse order
+    for (int i = N - 1; i >= 0; i--) {
+        cout << A[i] << " ";
     }
-    
+    cout << endl;
+
     return 0;
 }
+
+
+//five number...
+// struct Student {
+//     string name;
+//     int cls;
+//     char section;
+//     int id;
+//     int math_marks;
+//     int eng_marks;
+// };
+
+// bool compare(const Student &a, const Student &b) {
+//     if (a.eng_marks == b.eng_marks) {
+//         if (a.math_marks == b.math_marks) {
+//             return a.id < b.id;
+//         }
+//         return a.math_marks > b.math_marks;
+//     }
+//     return a.eng_marks > b.eng_marks;
+// }
+
+// int main() {
+//     int N;
+//     cin >> N;
+//     Student students[N];
+    
+//     for (int i = 0; i < N; ++i) {
+//         cin >> students[i].name >> students[i].cls >> students[i].section >> students[i].id >> students[i].math_marks >> students[i].eng_marks;
+//     }
+    
+//     sort(students, students + N, compare);
+    
+//     for (int i = 0; i < N; ++i) {
+//         cout << students[i].name << " " << students[i].cls << " " << students[i].section << " " << students[i].id << " " << students[i].math_marks << " " << students[i].eng_marks << endl;
+//     }
+    
+//     return 0;
+// }
 
 
 //four number...
